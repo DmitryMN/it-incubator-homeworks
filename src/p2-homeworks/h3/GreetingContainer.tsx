73e208b17dcbb623559d7eef85b1d5103fd27c3e
MalenkovDN <m18dm.dm@gmail.com>
2021-11-07 23:26:03 +0300

    ok

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -10,8 +10,6 @@ type GreetingContainerPropsType = {
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
 
-// более современный и удобный для про :)
-// уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
     const [name, setName] = useState<string>('')
     const [error, setError] = useState<string>('')
