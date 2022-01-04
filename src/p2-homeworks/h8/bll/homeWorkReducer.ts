4c8ea16e212dadd5eca6f113291ee724edfd93b4
MalenkovDN <m18dm.dm@gmail.com>
2022-01-05 01:34:10 +0300

    homework hw8 finished

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,20 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {ActionType, UserType} from "../HW8";
+
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            let copyState = state.map(user => ({...user}));
+            let collator = new Intl.Collator("ru");
+            copyState.sort((a: UserType, b: UserType) => {
+                return collator.compare(a.name, b.name);
+            });
+            return action.payload === 'up' ? copyState : copyState.reverse();
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(user => user.age > action.payload);
         }
-        default: return state
+        default:
+            return state
     }
 }
